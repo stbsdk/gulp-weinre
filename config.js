8d3677af7add44c383f611650f82267d57d2604c
Stanislav Kalashnik <sk@infomir.eu>
2016-01-16 15:12:12 +0200

    rework base config to extent root config

--- config.js
+++ config.js
@@ -1,23 +1,30 @@
 /**
  * WEb INspector REmote debugger server configuration for weinre gulp task.
+ *
+ * @author Stanislav Kalashnik <darkpark.main@gmail.com>
+ * @license GNU GENERAL PUBLIC LICENSE Version 3
  */
 
 'use strict';
 
-// public
-module.exports = {
-    // turn on/off server
-    active: true,
+var extend = require('extend'),
+    config = require('spa-gulp/config');
 
-    // listening HTTP port to provide client interface
-    port: 8080,
 
-    // address to listen
-    host: '-all-',
+// base config
+// each profile inherits all options from the "default" profile
+module.exports = extend(true, {}, config, {
+    default: {
+        // listening HTTP port to provide client interface
+        port: 8080,
 
-    // full logging
-    logging: false,
+        // address to listen
+        host: '-all-',
 
-    // debug servers session id
-    name: 'anonymous'
-};
+        // full logging
+        logging: false,
+
+        // debug servers session id
+        name: 'anonymous'
+    }
+});
